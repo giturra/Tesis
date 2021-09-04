d33cde2735127a2cef33a4576b8b2695de7ed444
Gabriel Iturra Bocaz <gabrieliturrab@ug.uchile.cl>
2021-09-03 20:21:54 -0400

    add word context matrix class

--- incremental_embedding.py
+++ incremental_embedding.py
@@ -1,24 +1,55 @@
 from river.base.transformer import Transformer
-from river.feature_extraction.vectorize import 
-# import Transformer
+from river.feature_extraction.vectorize import VectorizerMixin
 
-class IncrementalWordEmbedding(Transformer, VectorizeMixin):
+
+class IncrementalWordEmbedding(Transformer, VectorizerMixin):
 
     def __init__(
         self,
         v_size,
         c_size,
-        w_size
+        w_size,
         normalize=True,
-        on: str = None,
+        on=None,
         strip_accents=True,
         lowercase=True,
-        preprocessor: typing.Callable = None,
-        tokenizer: typing.Callable = None,
+        preprocessor=None,
+        tokenizer=None,
         ngram_range=(1, 1),
     ):
 
-    super().__init__(
+        super().__init__(
+            on=on,
+            strip_accents=strip_accents,
+            lowercase=lowercase,
+            preprocessor=preprocessor,
+            tokenizer=tokenizer,
+            ngram_range=ngram_range,
+        )
+        self.v_size = v_size
+        self.c_size = c_size
+        self.w_size = w_size
+
+
+class WordContextMatrix(IncrementalWordEmbedding):
+
+    def __init__(
+        self, 
+        v_size, 
+        c_size, 
+        w_size, 
+        normalize=True,
+        on=None,
+        strip_accents=True,
+        lowercase=True,
+        preprocessor=None,
+        tokenizer=None,
+        ngram_range=(1, 1),
+    ):
+        super().__init__(
+            v_size,
+            c_size,
+            w_size,
             on=on,
             strip_accents=strip_accents,
             lowercase=lowercase,
@@ -26,7 +57,9 @@ class IncrementalWordEmbedding(Transformer, VectorizeMixin):
             tokenizer=tokenizer,
             ngram_range=ngram_range,
     )
-    self.v_size = v_size
-    self.c_size = c_size
-    self.w_size = w_size
 
+    def transform_one(x):
+        ...
+
+
+wcm = WordContextMatrix(10, 5, 3)
