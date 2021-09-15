91936b89242ac159c9f28a3f571b696dd813fee9
Gabriel Iturra Bocaz <gabrieliturrab@ug.uchile.cl>
2021-09-15 17:40:19 -0300

    main file for run the code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from incremental_embedding import run, WordContextMatrix
 from river.datasets import SMSSpam
+from nltk import word_tokenize
 
 dataset = SMSSpam()
-wcm = WordContextMatrix(10_000, 100, 3)
+wcm = WordContextMatrix(10_000, 100, 3, is_ppmi=False)
 
-run(dataset, wcm, on='body') 
+run(dataset, wcm, on='body', tokenizer=word_tokenize)
